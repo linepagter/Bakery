f7adffc26236c120f91c84561cb3d304493c8974
Laura Emilie Svoldgaard Kristensen <au693851@uni.au.dk>
2024-03-10 13:00:53 +0100

    Model batch

--- Bakery/Models/Batch.cs
+++ Bakery/Models/Batch.cs
@@ -1,6 +1,27 @@
-﻿namespace Bakery.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Bakery.Models
+
 {
+    [Table("Batch")]
     public class Batch
     {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public DateTime start_time { get; set; }
+
+        [Required]
+
+        public DateTime finish_time { get; set; }
+
+        [Required]
+
+        public DateTime target_finish_time { get; set; }
     }
 }
