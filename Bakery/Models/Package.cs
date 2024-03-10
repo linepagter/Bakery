9ba6b090b46da4f7e7e501dce2c13938338ed74d
annelundkvist <anneyalundkvist@gmail.com>
2024-03-10 13:41:44 +0100

    Package and uses model updated

--- Bakery/Models/Package.cs
+++ Bakery/Models/Package.cs
@@ -1,6 +1,22 @@
-﻿namespace Bakery.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models
 {
+    [Table("Package")]
     public class Package
     {
+        
+        [Key]
+        [Required]
+        public int Trackid { get; set; }
+        
+        //Skal denne med?
+        [Required]
+        public int OrderId { get; set; }
+        
+        [Required]
+        public Order Order { get; set; }
     }
 }
--- Bakery/Models/Uses.cs
+++ Bakery/Models/Uses.cs
@@ -1,6 +1,24 @@
-﻿namespace Bakery.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models
 {
     public class Uses
     {
+        [Key]
+        [Required]
+        public int OrderId { get; set; }
+
+        [Key]
+        [Required]
+        public int IngredientId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+        
+        [Required]
+        public Order? Order { get; set; }
+
+        public Batch? Batch { get; set; }
+        
     }
 }
