843bc601f6aac23625683f7c94b71d81cd519279
linepagter <202105687@post.au.dk>
2024-03-11 15:41:36 +0100

    Name changing in models

--- Bakery/Controller/SeedController.cs
+++ Bakery/Controller/SeedController.cs
@@ -5,6 +5,7 @@ using CsvHelper;
 using System.Globalization;
 using Bakery.Data;
 using Microsoft.EntityFrameworkCore;
+using Bakery.Models.Csv;
 
 
 namespace Bakery.Controller
@@ -28,6 +29,7 @@ namespace Bakery.Controller
         [ResponseCache(NoStore = true)]
         public async Task<IActionResult> Put()
         {
+            //Setup
             var config = new CsvConfiguration(CultureInfo.GetCultureInfo("pt-BR"))
             {
                 HasHeaderRecord = true,
@@ -37,17 +39,33 @@ namespace Bakery.Controller
                 System.IO.Path.Combine(_environment.ContentRootPath, "Data/Bakery.csv"));
             using var csv = new CsvReader(reader, config);
             var existingBatch = await _context.Batch
-                .ToDictionaryAsync(batch => batch.Id);
+                .ToDictionaryAsync(batch => batch.BatchId);
             var existingIngredients = await _context.Ingredients
-                .ToDictionaryAsync(ingredients => ingredients.Id);
+                .ToDictionaryAsync(ingredients => ingredients.IngredientsId);
             var existingList_of_bakinggoods = await _context.ListOfBakingGoods
                 .ToDictionaryAsync(listofbakinggoods => listofbakinggoods.ListId);
             var existingOrder = await _context.Orders
-                .ToDictionaryAsync(orders => orders.Id);
+                .ToDictionaryAsync(orders => orders.OrderId);
             var existingPackage = await _context.Packages
                 .ToDictionaryAsync(package => package.Trackid);
             var existingStock = await _context.Stocks
-                .ToDictionaryAsync(stock => stock.Id);
+                .ToDictionaryAsync(stock => stock.StockId);
+            var now = DateTime.Now;
+
+            //Execute
+            var records = csv.GetRecords<BakeryRecord>();
+            var skippedRows = 0;
+            foreach (var record in records)
+            {
+                if (!record.Id.HasValue
+                    || string.IsNullOrEmpty(record.Name)
+                    ||existingBatch.ContainsKey(record.Id.Value))
+                { 
+                    skippedRows++;
+                    continue; 
+                }
+                    
+            }
 
         }
             
---
+++ Bakery/Models/Csv/BakeryRecord.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace Bakery.Models.Csv
+
+{
+    public class BakeryRecord
+    {
+        [Name("Id")]
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
