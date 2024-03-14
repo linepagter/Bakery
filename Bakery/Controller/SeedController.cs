6f7dc3f09907bd3531fec7bab539698f001b51d0
linepagter <202105687@post.au.dk>
2024-03-14 11:56:58 +0100

    Seedcontroller

--- Bakery/Controller/SeedController.cs
+++ Bakery/Controller/SeedController.cs
@@ -5,7 +5,7 @@ using CsvHelper;
 using System.Globalization;
 using Bakery.Data;
 using Microsoft.EntityFrameworkCore;
-using Bakery.Models.Csv;
+using Bakery.Models;
 
 
 namespace Bakery.Controller
@@ -26,8 +26,16 @@ namespace Bakery.Controller
         }
 
 
-        public void SeedBatch()
+        public void SeedPackage()
         {
+            Console.WriteLine("Seeding data");
+            var order1 = new Order { OrderId = 1, DeliveryPlace = "Finlandsgade 17", DeliveryDate = new DateTime(2024,08,12,08,00,00) };
+            
+            var package1 = new Package { TrackId = 12022, OrderId=1 };
+            
+            _context.Orders.Add(order1);
+            _context.Packages.Add(package1);
+            _context.SaveChanges();
 
         }
 
