786ec3921fb00afbf3aee547e0ec7246fd7c778b
linepagter <202105687@post.au.dk>
2024-03-14 11:17:46 +0100

    Seed method started

--- Bakery/Controller/SeedController.cs
+++ Bakery/Controller/SeedController.cs
@@ -24,50 +24,63 @@ namespace Bakery.Controller
             _context = context;
             _environment = environment;
         }
-        
-        // [HttpPut(Name = "Seed")]
-        // [ResponseCache(NoStore = true)]
-        // public async Task<IActionResult> Put()
-        // {
-        //     //Setup
-        //     var config = new CsvConfiguration(CultureInfo.GetCultureInfo("pt-BR"))
-        //     {
-        //         HasHeaderRecord = true,
-        //         Delimiter = ";",
-        //     };
-        //     using var reader = new StreamReader(
-        //         System.IO.Path.Combine(_environment.ContentRootPath, "Data/Bakery.csv"));
-        //     using var csv = new CsvReader(reader, config);
-        //     var existingBatch = await _context.Batch
-        //         .ToDictionaryAsync(batch => batch.BatchId);
-        //     var existingIngredients = await _context.Ingredients
-        //         .ToDictionaryAsync(ingredients => ingredients.IngredientsId);
-        //     var existingList_of_bakinggoods = await _context.ListOfBakingGoods
-        //         .ToDictionaryAsync(listofbakinggoods => listofbakinggoods.ListId);
-        //     var existingOrder = await _context.Orders
-        //         .ToDictionaryAsync(orders => orders.OrderId);
-        //     var existingPackage = await _context.Packages
-        //         .ToDictionaryAsync(package => package.Trackid);
-        //     var existingStock = await _context.Stocks
-        //         .ToDictionaryAsync(stock => stock.StockId);
-        //     var now = DateTime.Now;
-        //
-        //     //Execute
-        //     var records = csv.GetRecords<BakeryRecord>();
-        //     var skippedRows = 0;
-        //     foreach (var record in records)
-        //     {
-        //         if (!record.Id.HasValue
-        //             || string.IsNullOrEmpty(record.Name)
-        //             ||existingBatch.ContainsKey(record.Id.Value))
-        //         { 
-        //             skippedRows++;
-        //             continue; 
-        //         }
-        //             
-        //     }
-        //
-        // }
-            
+
+
+        public void SeedBatch()
+        {
+
+        }
+
+
+
+
+        //[HttpPut(Name = "Seed")]
+        //[ResponseCache(NoStore = true)]
+        //public async Task<IActionResult> Put()
+        //{
+
+
+
+
+        //    ////Setup
+        //    //var config = new CsvConfiguration(CultureInfo.GetCultureInfo("pt-BR"))
+        //    //{
+        //    //    HasHeaderRecord = true,
+        //    //    Delimiter = ";",
+        //    //};
+        //    //using var reader = new StreamReader(
+        //    //    System.IO.Path.Combine(_environment.ContentRootPath, "Data/Bakery.csv"));
+        //    //using var csv = new CsvReader(reader, config);
+        //    //var existingBatch = await _context.Batch
+        //    //    .ToDictionaryAsync(batch => batch.BatchId);
+        //    //var existingIngredients = await _context.Ingredients
+        //    //    .ToDictionaryAsync(ingredients => ingredients.IngredientsId);
+        //    //var existingList_of_bakinggoods = await _context.ListOfBakingGoods
+        //    //    .ToDictionaryAsync(listofbakinggoods => listofbakinggoods.ListId);
+        //    //var existingOrder = await _context.Orders
+        //    //    .ToDictionaryAsync(orders => orders.OrderId);
+        //    //var existingPackage = await _context.Packages
+        //    //    .ToDictionaryAsync(package => package.Trackid);
+        //    //var existingStock = await _context.Stocks
+        //    //    .ToDictionaryAsync(stock => stock.StockId);
+        //    //var now = DateTime.Now;
+
+        //    ////Execute
+        //    //var records = csv.GetRecords<BakeryRecord>();
+        //    //var skippedRows = 0;
+        //    //foreach (var record in records)
+        //    //{
+        //    //    if (!record.Id.HasValue
+        //    //        || string.IsNullOrEmpty(record.Name)
+        //    //        || existingBatch.ContainsKey(record.Id.Value))
+        //    //    {
+        //    //        skippedRows++;
+        //    //        continue;
+        //    //    }
+
+        //    //}
+
+        //}
+
     }
 }
