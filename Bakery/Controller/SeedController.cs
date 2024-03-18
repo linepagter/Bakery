968b03e320b2ee1fd33f8e938a859b4be1c71171
nikolinehoeyiversen <nikohoey@gmail.com>
2024-03-18 15:09:26 +0100

    seedController

--- Bakery/Controller/SeedController.cs
+++ Bakery/Controller/SeedController.cs
@@ -10,9 +10,9 @@ using Bakery.Models;
 
 namespace Bakery.Controller
 {
-    [Microsoft.AspNetCore.Components.Route("[controller")]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
     [ApiController]
-    public class SeedController
+    public class SeedController:ControllerBase
     {
          private readonly MyDbContext _context;
          private IWebHostEnvironment _environment;
@@ -32,9 +32,9 @@ namespace Bakery.Controller
         {
             var skippedRows = 0;
             Console.WriteLine("Seeding data");
-            var order1 = new Order { OrderId = 1, DeliveryPlace = "Finlandsgade 17", DeliveryDate = new DateTime(2024,08,12,08,00,00) };
+            var order1 = new Order { DeliveryPlace = "Finlandsgade 17", DeliveryDate = new DateTime(2024,08,12,08,00,00) };
             
-            var package1 = new Package { TrackId = 12022, OrderId=1 };
+            var package1 = new Package { Order=order1};
             
             _context.Orders.Add(order1);
             _context.Packages.Add(package1);
@@ -43,8 +43,7 @@ namespace Bakery.Controller
             return new JsonResult(new
             {
                 Package = _context.Packages.Count(),
-                Order = _context.Orders.Count(),
-                SkippedRows = skippedRows
+                Order = _context.Orders.Count()
                 
             });
 
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -27,67 +27,10 @@ namespace Bakery
             builder.Services.AddSwaggerGen();
             
             var app = builder.Build();
-            
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.MapControllers();
-            
-            if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
-                app.UseDeveloperExceptionPage();
-            else
-                app.UseExceptionHandler("/error");
-            
-
-            //app.UseCors();
-
-            app.UseAuthorization();
-
-            // Minimal API
-            // app.MapGet("/error",
-            //     [EnableCors("AnyOrigin")]
-            //     [ResponseCache(NoStore = true)] (HttpContext context) =>
-            //     {
-            //         var exceptionHandler =
-            //             context.Features.Get<IExceptionHandlerPathFeature>();
-            //
-            //         var details = new ProblemDetails();
-            //         details.Detail = exceptionHandler?.Error.Message;
-            //         details.Extensions["traceId"] =
-            //             System.Diagnostics.Activity.Current?.Id
-            //             ?? context.TraceIdentifier;
-            //         details.Type =
-            //             "https://tools.ietf.org/html/rfc7231#section-6.6.1";
-            //         details.Status = StatusCodes.Status500InternalServerError;
-            //         return Results.Problem(details);
-            //     });
-            //
-            // app.MapGet("/error/test",
-            //     [EnableCors("AnyOrigin")]
-            //     [ResponseCache(NoStore = true)] () =>
-            //     { throw new Exception("test"); });
-            //
-            // app.MapGet("/cod/test",
-            //     [EnableCors("AnyOrigin")]
-            //     [ResponseCache(NoStore = true)] () =>
-            //         Results.Text("<script>" +
-            //                      "window.alert('Your client supports JavaScript!" +
-            //                      "\\r\\n\\r\\n" +
-            //                      $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
-            //                      "\\r\\n" +
-            //                      "Client time (UTC): ' + new Date().toISOString());" +
-            //                      "</script>" +
-            //                      "<noscript>Your client does not support JavaScript</noscript>",
-            //             "text/html")); //CHANGE
-
-            // Controllers
-            //app.MapControllers().RequireCors("AnyOrigin");
-
 
             app.Run();
         }
