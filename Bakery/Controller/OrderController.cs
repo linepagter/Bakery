1c36c5e0e8bf28a0842e0af6e9647ac1eaa69731
nikolinehoeyiversen <nikohoey@gmail.com>
2024-03-21 10:48:49 +0100

    OrderController

--- Bakery/Controller/OrderController.cs
+++ Bakery/Controller/OrderController.cs
@@ -16,81 +16,22 @@ public class OrderController: ControllerBase
         _context = context;
     }
     
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
-        {
-            return await _context.Orders.ToListAsync();
-        }
+    [HttpGet("Query2")]
+    public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+    {
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrder(int id)
-        {
-            var order = await _context.Orders.FindAsync(id);
+        List<int> orderIds = new List<int> { 4 };
 
-            if (order == null)
+        var query = from o in _context.Orders
+            where orderIds.Contains(o.OrderId)
+            select new
             {
-                return NotFound();
-            }
+                DeliveryAdress = o.DeliveryPlace,
+                DeliveryDate = o.DeliveryDate
+            };
 
-            return order;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
-        {
-            _context.Orders.Add(order);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetOrder", new { id = order.OrderId }, order);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder(int id, Order order)
-        {
-            if (id != order.OrderId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteOrder(int id)
-        {
-            var order = await _context.Orders.FindAsync(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool OrderExists(int id)
-        {
-            return _context.Orders.Any(e => e.OrderId == id);
-        }
+        var result = query.ToList();
+        return Ok(result);
+    }
     
 }
