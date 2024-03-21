30ae80c82cf3f23978a487a2cf2ae129c5d9d0cc
nikolinehoeyiversen <nikohoey@gmail.com>
2024-03-21 16:48:23 +0100

    BakingGoodsController

--- Bakery/Controller/BakingGoodsController.cs
+++ Bakery/Controller/BakingGoodsController.cs
@@ -19,82 +19,36 @@ public class BakingGoodsController: ControllerBase
         _context = context;
     }
     
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<BakingGood>>> GetListOfBakingGoods()
-        {
-            return await _context.BakingGoods.ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BakingGood>> GetListOfBakingGoods(int id)
-        {
-            var listOfBakingGoods = await _context.BakingGoods.FindAsync(id);
-
-            if (listOfBakingGoods == null)
+    [HttpGet("Query3")]
+    public async Task<ActionResult<IEnumerable<BakingGood>>> GetBakedGoods(int orderId)
+    {
+        var query = from bg in _context.BakingGoods
+            join bgo in _context.BakingGoodOrders on bg.BakingGoodId equals bgo.BakingGoodId
+            where bgo.OrderId == orderId
+            select new
             {
-                return NotFound();
-            }
+                BakingName = bg.BakingGoodName,
+                Quantity = bg.Quantity
+            };
 
-            return listOfBakingGoods;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<BakingGood>> PostListOfBakingGoods(BakingGood bakingGood)
-        {
-            _context.BakingGoods.Add(bakingGood);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetListOfBakingGoods", new { id = bakingGood.BakingGoodId }, bakingGood);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutListOfBakingGoods(int id, BakingGood bakingGood)
-        {
-            if (id != bakingGood.BakingGoodId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(bakingGood).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ListOfBakingGoodsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteListOfBakingGoods(int id)
-        {
-            var listOfBakingGoods = await _context.BakingGoods.FindAsync(id);
-            if (listOfBakingGoods == null)
-            {
-                return NotFound();
-            }
-
-            _context.BakingGoods.Remove(listOfBakingGoods);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ListOfBakingGoodsExists(int id)
-        {
-            return _context.BakingGoods.Any(e => e.BakingGoodId == id);
-        }
+        var result = await query.ToListAsync();
+        return Ok(result);
+    }
     
+    [HttpGet("Query6")]
+    public async Task<ActionResult<IEnumerable<BakingGood>>> GetAll()
+    {
+        var query = from bg in _context.BakingGoods
+            group bg by bg.BakingGoodName into g
+            orderby g.Key ascending
+            select new
+            {
+                BakingName = g.Key,
+                TotalQuantity = g.Sum(bg => bg.Quantity)
+            };
+
+        var result = await query.ToListAsync();
+        return Ok(result);
+    }
     
 }
