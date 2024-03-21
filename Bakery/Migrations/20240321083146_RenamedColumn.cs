96ad9441ba3ea5643f4c8354a171038ff8775bc8
annelundkvist <anneyalundkvist@gmail.com>
2024-03-21 09:43:16 +0100

    new miigrations

---
+++ Bakery/Migrations/20240321083146_RenamedColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bakery.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "Ingredients",
+                newName: "StockQuantity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockQuantity",
+                table: "Ingredients",
+                newName: "Quantity");
+        }
+    }
+}
