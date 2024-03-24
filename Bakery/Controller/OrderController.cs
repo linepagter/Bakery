a927904c24bcc0998328f689161425cfdda8c9d1
linepagter <202105687@post.au.dk>
2024-03-24 18:44:48 +0100

    change made to fit firsrt seeding

--- Bakery/Controller/OrderController.cs
+++ Bakery/Controller/OrderController.cs
@@ -20,7 +20,7 @@ public class OrderController: ControllerBase
     public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
     {
 
-        int orderId =  3 ;
+        int orderId =  1 ;
 
         var query = from o in _context.Orders
             where orderId.Equals(o.OrderId)
